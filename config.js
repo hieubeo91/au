7ad60178574fa04508cf0cfadd3d2b93ea5153ea
hieubeo91 <hieubeohp91@gmail.com>
2026-07-05 06:23:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // BACKGROUND
-const GLOBAL_BG_IMAGE = "https://lh3.googleusercontent.com/d/1LFsk3iGkeZAOZ0cQ16hx0WsmAhpxdh2C";
+const GLOBAL_BG_IMAGE = "https://lh3.googleusercontent.com/d/18-TpyUz6uPU2YZOClxlHwnzKTFXwtujH";
