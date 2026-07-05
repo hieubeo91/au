ecbf31a72999baf4fd51063d232440760bff86b8
hieubeo91 <hieubeohp91@gmail.com>
2026-07-05 19:30:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,24 @@
 // BACKGROUND 
 const GLOBAL_BG_IMAGE = "https://lh3.googleusercontent.com/d/18-TpyUz6uPU2YZOClxlHwnzKTFXwtujH";
+
+// SLIDEBAR
+const BANNER_SLIDES_CONFIG = [
+    {
+        link: "https://hieubeo91.github.io/au/posts/sun3.html",
+        image: "https://lh3.googleusercontent.com/d/18-TpyUz6uPU2YZOClxlHwnzKTFXwtujH",
+        title: "Thang cấp độ nhẫn trong Audition",
+        altText: "Bài viết mới 1"
+    },
+    {
+        link: "https://hieubeo91.github.io/au/posts/sun3.html",
+        image: "https://lh3.googleusercontent.com/d/1BKluvvJ1YrQS3FqGu8jV3Qn8zqkr_QAD",
+        title: "Thang cấp độ level trong Audition",
+        altText: "Bài viết mới 2"
+    },
+    {
+        link: "https://hieubeo91.github.io/au/posts/sun3.html",
+        image: "https://lh3.googleusercontent.com/d/1luXtV6Aq4_PszysrMwWfH27VZ6jlb0Ki",
+        title: "Hướng dẫn viết tiếng Việt trong Audition",
+        altText: "Bài viết mới 3"
+    }
+];
