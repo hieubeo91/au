f62bffc29380224d5a57a8b03e2ced0313117b9f
hieubeo91 <hieubeohp91@gmail.com>
2026-07-06 00:03:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -28,6 +28,6 @@ const MY_SITE_CONFIG = {
     admin: {
         secureTargetHash: "57a81a4801bb04e890c8414a2ff4b6ea30dcdf25b5eec1c0e4eec8fbf3bd5181",
         displayName: "Hiệu Béo 👑"
-        classes: ['border-emerald-500', 'bg-emerald-50/30', 'text-emerald-700', 'font-bold']
+        classes: ['border-red-500', 'bg-red-50/30', 'text-red-700', 'font-bold']
     }
 };
